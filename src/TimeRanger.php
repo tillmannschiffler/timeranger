78c60d49fae082e6186f0c264032478afbf0fbe2
Tillmann Schiffler <tillmann.schiffler@gmail.com>
2022-03-23 10:56:11 +0100

    go on

--- src/TimeRanger.php
+++ src/TimeRanger.php
@@ -18,13 +18,71 @@ class TimeRanger
      */
     public function __construct(DateTimeImmutable $start, DateTimeImmutable $end)
     {
-        if ($end < $start) 
-        {
-            throw new TimeRangerException('start is bigger than end');
-        }
+        $this->ensureRangeIsValid($start, $end);
         
         $this->start = $start;
         $this->end = $end;
+    }
+
+    /**
+     * checks if range overlaps
+     * 
+     * @param TimeRanger $other
+     * @return bool
+     */
+    public function overlap(self $other) : bool
+    {
+        if (($other->getStart() <= $this->getEnd()) and ($other->getEnd() >= $this->getStart()))
+        {
+            return true;
+        }
+            
+        return false;
+    }
+
+    /**
+     * checks if range is equal
+     * 
+     * @param TimeRanger $other
+     * @return bool
+     */
+    public function equals(self $other): bool
+    {
+        if (($this->getStart() == $other->getStart()) and ($this->getEnd() == $other->getEnd()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+    
+    /**
+     * @throws TimeRangerException
+     */
+    public static function createYesterday(): self
+    {
+        return new self(
+            new DateTimeImmutable('yesterday 0:0:0'),
+            new DateTimeImmutable('yesterday 23:59:59')
+        );
+    }
+
+    /**
+     * Guard this
+     * 
+     * @param DateTimeImmutable $start
+     * @param DateTimeImmutable $end
+     * @return void
+     * @throws TimeRangerException
+     */
+    private function ensureRangeIsValid(DateTimeImmutable $start, DateTimeImmutable $end) : void
+    {
+        if ($end > $start)
+        {
+            return;
+        }
+
+        throw new TimeRangerException('start is bigger than end');
     }
 
     /**
---
+++ tests/unit/TimeRangerTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace unit;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use timeranger\TimeRanger;
+use timeranger\TimeRangerException;
+
+/**
+ * @covers \timeranger\TimeRanger
+ */
+class TimeRangerTest extends TestCase
+{
+    /**
+     * @throws \timeranger\TimeRangerException
+     */
+    public function testCanCreate(): void
+    {
+        $this->assertInstanceOf(
+            TimeRanger::class,
+            new TimeRanger(
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+            )
+        );
+    }
+
+    public function testCantCreateWithInvalid(): void
+    {
+        $this->expectException(TimeRangerException::class);
+
+        $this->assertInstanceOf(
+            TimeRanger::class,
+            new TimeRanger(
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01'),
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00')
+            )
+        );
+    }
+
+    public function testCanReturnsTrueOnEquals(): void
+    {
+        $rangeA = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+        );
+
+        $rangeB = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+        );
+
+        $this->assertTrue($rangeA->equals($rangeB));
+    }
+
+    public function testCanReturnsFalseOnEquals(): void
+    {
+        $rangeA = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+        );
+
+        $rangeB = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:02')
+        );
+
+        $this->assertFalse($rangeA->equals($rangeB));
+    }
+
+    public function testCantCreateWithSame(): void
+    {
+        $this->expectException(TimeRangerException::class);
+
+        $this->assertInstanceOf(
+            TimeRanger::class,
+            new TimeRanger(
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00')
+            )
+        );
+    }
+
+    /**
+     * @throws \timeranger\TimeRangerException
+     */
+    public function testCanGetStart(): void
+    {
+        $range = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+        );
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $range->getStart());
+    }
+
+    /**
+     * @throws \timeranger\TimeRangerException
+     */
+    public function testCanGetEnd(): void
+    {
+        $range = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:01')
+        );
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $range->getEnd());
+    }
+
+    /**
+     * @dataProvider provideRanges
+     */
+    public function testCanCheckForOverlap($expected, $other): void
+    {
+        $rangeA = new TimeRanger(
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 14:00:00'),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 14:59:59')
+        );
+
+        $this->assertEquals($expected, $rangeA->overlap($other));
+    }
+
+    public function provideRanges(): array
+    {
+        return [
+            [
+                false,
+                new TimeRanger(
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 13:00:00'),
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 13:59:59')
+                ),
+            ],
+            [
+                false,
+                new TimeRanger(
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 15:00:00'),
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 15:59:59')
+                ),
+            ],
+            [
+                true,
+                new TimeRanger(
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 11:00:00'),
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 14:00:03')
+                ),
+            ],
+            [
+                true,
+                new TimeRanger(
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 14:59:59'),
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 15:00:03')
+                ),
+            ],
+            [
+                true,
+                new TimeRanger(
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 10:59:59'),
+                    DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-01-01 16:00:03')
+                ),
+            ],
+        ];
+    }
+}
