7c16add7ae717baecaf64a3303d2e8afa005c559
Tillmann Schiffler <tillmann.schiffler@gmail.com>
2022-06-03 11:40:20 +0200

    cleanup and reformat

--- src/TimeRanger.php
+++ src/TimeRanger.php
@@ -19,43 +19,41 @@ class TimeRanger
     public function __construct(DateTimeImmutable $start, DateTimeImmutable $end)
     {
         $this->ensureRangeIsValid($start, $end);
-        
+
         $this->start = $start;
         $this->end = $end;
     }
 
     /**
      * checks if range overlaps
-     * 
+     *
      * @param TimeRanger $other
      * @return bool
      */
-    public function overlap(self $other) : bool
+    public function overlap(self $other): bool
     {
-        if (($other->getStart() <= $this->getEnd()) and ($other->getEnd() >= $this->getStart()))
-        {
+        if (($other->getStart() <= $this->getEnd()) and ($other->getEnd() >= $this->getStart())) {
             return true;
         }
-            
+
         return false;
     }
 
     /**
      * checks if range is equal
-     * 
+     *
      * @param TimeRanger $other
      * @return bool
      */
     public function equals(self $other): bool
     {
-        if (($this->getStart() == $other->getStart()) and ($this->getEnd() == $other->getEnd()))
-        {
+        if (($this->getStart() == $other->getStart()) and ($this->getEnd() == $other->getEnd())) {
             return true;
         }
 
         return false;
     }
-    
+
     /**
      * @throws TimeRangerException
      */
@@ -69,16 +67,15 @@ class TimeRanger
 
     /**
      * Guard this
-     * 
+     *
      * @param DateTimeImmutable $start
      * @param DateTimeImmutable $end
      * @return void
      * @throws TimeRangerException
      */
-    private function ensureRangeIsValid(DateTimeImmutable $start, DateTimeImmutable $end) : void
+    private function ensureRangeIsValid(DateTimeImmutable $start, DateTimeImmutable $end): void
     {
-        if ($end > $start)
-        {
+        if ($end > $start) {
             return;
         }
 
